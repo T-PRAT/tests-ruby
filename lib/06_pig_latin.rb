ccd3403825d80d94d8755e1a6f8df64b5a0bf24a
tprat <Titouan.p@hotmail.fr>
2021-10-12 01:59:48 +0200

    finished :)

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,3 +1,16 @@
 def translate(str)
-
+	v = ['a','e','i','o','u']
+	split = str.split
+	split.each do |word|
+		3.times do
+			if word[0..1] == "qu"
+				(word << word[0..1]).slice!(0..1)
+			end
+			if !v.include? word[0]
+				(word << word[0]).slice!(0)
+			end
+		end
+		word << "ay"
+	end
+	split.join(' ')
 end
